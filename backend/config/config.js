c0f99c22847a2793003e6cf20348e1e046780f5b
Tri Ho <trihodac66@gmail.com>
2025-03-26 17:38:43 -0700

    chore: created .env and move all related variable to there

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,19 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import dotenv from "dotenv"
+
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA3W4jbB2qEqCqXb9kI7UCE43B0eEUuXas",
-  authDomain: "tabmark-d081a.firebaseapp.com",
-  projectId: "tabmark-d081a",
-  storageBucket: "tabmark-d081a.firebasestorage.app",
-  messagingSenderId: "954155850318",
-  appId: "1:954155850318:web:ecfdef2ffeefc1d355411f",
-  measurementId: "G-2K8B7BGKH3"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
--- backend/services/sendGridService.js
+++ backend/services/sendGridService.js
@@ -1,26 +1,26 @@
-// const sgMail = require('@sendgrid/mail');
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);  // Use environment variable for API key
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);  // Use environment variable for API key
 
-// /**
-//  * Function to send an email via SendGrid
-//  * @param {string} to - Recipient email address
-//  * @param {string} subject - Email subject
-//  * @param {string} text - Email body content
-//  */
-// const sendEmail = async (to, subject, text) => {
-//   const msg = {
-//     to: to, // Email from OAuth
-//     from: 'support@tabmark.com', // Verified sender email on SendGrid
-//     subject: subject,
-//     text: text, // Body of the email
-//   };
+/**
+ * Function to send an email via SendGrid
+ * @param {string} to - Recipient email address
+ * @param {string} subject - Email subject
+ * @param {string} text - Email body content
+ */
+const sendEmail = async (to, subject, text) => {
+  const msg = {
+    to: to, // Email from OAuth
+    from: 'support@tabmark.com', // Verified sender email on SendGrid
+    subject: subject,
+    text: text, // Body of the email
+  };
 
-//   try {
-//     await sgMail.send(msg);
-//     console.log('Email sent successfully');
-//   } catch (error) {
-//     console.error('Error sending email:', error);
-//   }
-// };
+  try {
+    await sgMail.send(msg);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
 
-// module.exports = { sendEmail };
+module.exports = { sendEmail };
